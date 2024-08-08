13927ed3fe9c51c453ae7e36aecca48e0e9d4e42
Walen <weilinchiang@gmail.com>
2024-08-08 15:19:59 +0800

    第二次前端更新:註冊

--- First-Project/src/App.jsx
+++ First-Project/src/App.jsx
@@ -1,14 +1,26 @@
 import Navbars from "./Components/Navbars"
+import Validations from "./Components/Validations"
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+
 
 function Navbar() {
   return <Navbars />
 }
 
+function Validation() {
+  return <Validations />
+}
 
 function App() {
   return (
-    <Navbar/>
-    
+    <Router>
+    <div>
+      <Navbars />
+    <Routes>
+    <Route path="/register" element={<Validation/>} />
+    </Routes>
+    </div>
+    </Router>
   )
 }
 
--- First-Project/src/Components/Navbars.jsx
+++ First-Project/src/Components/Navbars.jsx
@@ -8,16 +8,17 @@ function Navbars() {
   return (
     <Navbar bg="dark" data-bs-theme="dark" expand="lg" className="bg-body-tertiary">
       <Container>
+        <h1>聯大二手書交易站</h1>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
             <Nav.Link href="#Home" className='k'>首頁</Nav.Link>  
             <Nav.Link href="#Browse">瀏覽</Nav.Link>
-            <Nav.Link href="#Register">註冊</Nav.Link>
+            <Nav.Link href="/register">註冊</Nav.Link>
             <Nav.Link href="#Sign_in">登入</Nav.Link>
             <NavDropdown title="選單" id="#Menu">
               <NavDropdown.Item href="#Menu/Publish">刊登</NavDropdown.Item>
-              <NavDropdown.Item href="#Menu/Check">查看已刊登商品</NavDropdown.Item>
+              <NavDropdown.Item href="#Validations.jsx">查看已刊登商品</NavDropdown.Item>
             </NavDropdown>
           </Nav>
         </Navbar.Collapse>
