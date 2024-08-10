e30f5dbc31bab9ac1faea060d7218573fe994261
Walen <weilinchiang@gmail.com>
2024-08-11 01:16:42 +0800

    第七次前端更新:針對架站開啟進行css調整

--- First-Project/src/App.jsx
+++ First-Project/src/App.jsx
@@ -2,7 +2,7 @@ import Cards from "./Components/Cards";
 import Navbars from "./Components/Navbars";
 import Validations from "./Components/Validations";
 import Accordions from "./Components/Accordions";
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { HashRouter as Router, Route, Routes } from 'react-router-dom';
 import { Login1 } from "./Components/Login1";
 import { Sign1 } from "./Components/Sign1";
 
@@ -32,16 +32,16 @@ function Accordion() {
 function App() {
   return (
     <Router>
-    <div>
-      <Navbar />
-    <Routes>
-    <Route path="/" element={<Accordion/>} />
-    <Route path="/home" element={<Accordion/>} />
-    <Route path="/card" element={<Cards/>} />
-    <Route path="/sign" element={<Sign1/>} />
-    <Route path="/login" element={<Login1/>} />
-    </Routes>
-    </div>
+      <div>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Accordion />} />
+          <Route path="/home" element={<Accordion />} />
+          <Route path="/card" element={<Cards />} />
+          <Route path="/sign" element={<Sign1 />} />
+          <Route path="/login" element={<Login1 />} />
+        </Routes>
+      </div>
     </Router>
   )
 }
--- First-Project/src/Components/Navbars.jsx
+++ First-Project/src/Components/Navbars.jsx
@@ -7,24 +7,23 @@ import './Navbars.css';
 function Navbars() {
   return (
     <Navbar bg="dark" data-bs-theme="dark" expand="lg" className="bg-body-tertiary">
-      <Container>
-        <h1 className='p'>聯大二手書交易站</h1>
+      <Container fluid>
+        <Navbar.Brand className='navbar-brand'>聯大二手書交易站</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
-            <Nav.Link href="/home">首頁</Nav.Link>  
-            <Nav.Link href="/card">瀏覽</Nav.Link>
-            <Nav.Link href="/sign">註冊</Nav.Link>
-            <Nav.Link href="/login">登入</Nav.Link>
-            <NavDropdown title="選單" id="#Menu">
+            <Nav.Link href="/#home">首頁</Nav.Link>  
+            <Nav.Link href="/#card">瀏覽</Nav.Link>
+            <Nav.Link href="/#sign">註冊</Nav.Link>
+            <Nav.Link href="/#login">登入</Nav.Link>
+            <NavDropdown title="選單" id="basic-nav-dropdown">
               <NavDropdown.Item href="#Menu/Publish">刊登</NavDropdown.Item>
-              <NavDropdown.Item href="#Validations.jsx">查看已刊登商品</NavDropdown.Item>
+              <NavDropdown.Item href="#Validations">查看已刊登商品</NavDropdown.Item>
             </NavDropdown>
           </Nav>
         </Navbar.Collapse>
       </Container>
     </Navbar>
-    
   );
 }
 
